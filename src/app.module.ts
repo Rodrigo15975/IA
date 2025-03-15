0bddfe8067b49a636cea029b854ea637aa0e95dd
rodrigo15975 <javierluis15975@hotmail.com>
2025-03-14 22:15:36 -0500

    update app.module.ts

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,10 +3,11 @@ import { AppController } from './app.controller'
 import { AppService } from './app.service'
 import { HuggingfaceService } from './huggingface/huggingface.service'
 import { ConfigModule } from '@nestjs/config'
+import { ChatBotsService } from './huggingface/chat-bot.service'
 
 @Module({
   imports: [ConfigModule.forRoot()],
   controllers: [AppController],
-  providers: [AppService, HuggingfaceService],
+  providers: [AppService, HuggingfaceService, ChatBotsService],
 })
 export class AppModule {}
