0defa557f6f9a0ee105921c14325c82a19437051
rodrigo15975 <javierluis15975@hotmail.com>
2025-03-16 04:21:29 -0500

    feat: create templatePrompt.ts

---
+++ src/bot/common/template/templatePrompt.ts
@@ -0,0 +1,2 @@
+export const ASISTENTE_TEMPLATE =
+  'Eres un asistente útil, hábil que proporciona respuetas precisas. Responde a la siguiente consulta: {input}\n\nHistorial de la conversación: {history}'
